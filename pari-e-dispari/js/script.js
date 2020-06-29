8914fa52514044f0ce2f90a167f990810768a00e
Davit <davit.janashia1@gmail.com>
2020-06-29 18:09:03 +0200

    iniziato pari dispari

---
+++ pari-e-dispari/js/script.js
@@ -0,0 +1,5 @@
+var btn = document.getElementById('my-btn');
+btn.addEventListener('click', function(){
+  var utente = parseInt(prompt('inserisci un numero da 1 a 5'));
+  console.log(utente);
+});
