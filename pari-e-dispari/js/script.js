7ef22ac031297b4362bf9b4181ef917a872ef198
Davit <davit.janashia1@gmail.com>
2020-06-29 18:58:11 +0200

    da fare funzioni

--- pari-e-dispari/js/script.js
+++ pari-e-dispari/js/script.js
@@ -1,5 +1,19 @@
 var btn = document.getElementById('my-btn');
 btn.addEventListener('click', function(){
   var utente = parseInt(prompt('inserisci un numero da 1 a 5'));
-  console.log(utente);
+  document.getElementById('n-tuo').innerHTML = 'il tuo numero: ' + utente;
+  var cpu = Math.ceil(Math.random()*5);
+  document.getElementById('n-cpu').innerHTML = 'numero cpu: ' + cpu;
+
+  var somma = utente + cpu;
+  document.getElementById('somma').innerHTML = 'la somma é ' + somma;
+
+  var option = document.getElementById('my-opt').value;
+  console.log(option);
+
+  if ((option == 'pari' && somma % 2 == 0) || (option == 'dispari' && somma % 2 == 1)){
+    document.getElementById('result').innerHTML = 'hai vinto';
+  } else if ((option == 'pari' && somma % 2 == 1) || (option == 'dispari' && somma % 2 == 0)) {
+    document.getElementById('result').innerHTML = 'hai perso';
+  }
 });
